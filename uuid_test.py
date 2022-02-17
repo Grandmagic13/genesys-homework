7dbed1980e2a1b26e1f5f6939a137dfa85ddebf5
Grandmagic13 <jabencefear@hotmail.com>
2022-02-17 01:10:31 +0100

    Add: Second test for validating schema

--- uuid_test.py
+++ uuid_test.py
@@ -1,17 +1,32 @@
 import unittest
+import requests
+import json
 
 from common import BASE_URL
+from jsonschema import validate
 from hamcrest import *
-import requests
 
 UUID_ENDPOINT_URL = "{0}/{1}".format(BASE_URL, "uuid")
+
 
 class UUIDTests(unittest.TestCase):
     def test_get_uuid_endpoint_returns_200_ok(self):
         response = requests.get(UUID_ENDPOINT_URL)
         assert_that(response.status_code, is_(200))
 
-    # test response body contains uuid
+    def test_get_uuid_endpoint_schema_validity(self):
+        schema = {
+            "type": "object",
+            "properties": {
+                "uuid": {
+                    "type": "string",
+                    "format": "uuid"
+                }
+            }
+        }
+        response = requests.get(UUID_ENDPOINT_URL)
+        json_data = json.loads(response.text)
+        validate(instance=json_data, schema=schema)
 
 if __name__ == '__main__':
     unittest.main()
